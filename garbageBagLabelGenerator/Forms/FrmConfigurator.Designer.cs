d91875b9e117fc86208f53f8f6bb3bc1b9ae2452
Maciej Sikorski <sikorskim@SzpitalwSremie.lokalne>
2019-07-17 11:47:12 +0200

    some changes

--- garbageBagLabelGenerator/Forms/FrmConfigurator.Designer.cs
+++ garbageBagLabelGenerator/Forms/FrmConfigurator.Designer.cs
@@ -639,6 +639,7 @@
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(69, 20);
             this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
             // 
             // label11
             // 
--- garbageBagLabelGenerator/Forms/FrmConfigurator.cs
+++ garbageBagLabelGenerator/Forms/FrmConfigurator.cs
@@ -20,6 +20,8 @@ namespace garbageBagLabelGenerator.Forms
         void startup()
         {
             getFonts();
+            numericUpDown2.Minimum = 1;
+            numericUpDown3.Minimum = 1;
         }
 
         void getFonts()
@@ -74,6 +76,57 @@ namespace garbageBagLabelGenerator.Forms
             pictureBox1.Update();
         }
 
+        void drawPage()
+        {
+
+            int width = Int32.Parse(textBox5.Text);
+            int height = Int32.Parse(textBox4.Text);
+            int columns = (int)numericUpDown2.Value;
+            int rows = (int)numericUpDown3.Value;
+
+            int marginLeft = (int)numericUpDown6.Value;
+            int marginTop = (int)numericUpDown4.Value;
+
+            Image img = new Bitmap(pictureBox3.Width, pictureBox3.Height);
+            Graphics g = Graphics.FromImage(img);
+            Pen p = new Pen(Color.Black, 2);
+            Rectangle rectangle = new Rectangle(0, 0, width, height);
+            Brush brush = new SolidBrush(Color.White);
+            g.FillRectangle(brush, rectangle);
+            g.DrawRectangle(p, rectangle);
+
+            int labelWidth = Int32.Parse(textBox1.Text);
+            int labelHeight = Int32.Parse(textBox2.Text);
+
+            //draw label
+            int currentColumn = 0;
+            int currentRow = 0;
+
+            Pen labelBorder = new Pen(Color.White, 0);
+
+            while (currentRow < rows)
+            {
+                Point labelLocation = new Point();
+                //labelLocation = new Point(Label.Size.Width * currentColumn, Label.Size.Height * currentRow);
+                labelLocation.X = labelWidth * currentColumn + marginLeft - 5;
+                labelLocation.Y = labelWidth * currentRow + marginTop;
+                Rectangle label = new Rectangle(labelLocation, new Size(labelWidth,labelHeight));
+                int labelCenter = labelLocation.X + width / 2;
+                g.DrawRectangle(labelBorder, label);
+
+                currentColumn++;
+                if (currentColumn % columns == 0)
+                {
+                    currentColumn = 0;
+                    currentRow++;
+                }
+            }
+
+            pictureBox3.Image = img;
+            pictureBox1.SizeMode=PictureBoxSizeMode.Normal;
+            pictureBox3.Update();
+        }
+
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
             drawLabel();
@@ -87,6 +140,11 @@ namespace garbageBagLabelGenerator.Forms
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
             drawLabel();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            drawPage();
         }
     }
 }
